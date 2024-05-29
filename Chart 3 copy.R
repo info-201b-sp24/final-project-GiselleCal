3b9201e7be1ee4ad2df145978d0b8bf6ef5f968d
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-28 19:27:06 -0700

    updated

---
+++ Chart 3 copy.R
@@ -0,0 +1,19 @@
+# Load ggplot2
+library(ggplot2)
+
+foundation_dataset <- read.csv("shades.csv")
+
+#filtering data just to get the two brands!
+ 
+filtered_data <- subset(foundation_dataset, brand %in% c("Fenty", "MAC"))
+
+# Bar graph: Comparing the difference saturation levels between Fenty and Mac. 
+
+ggplot(data = filtered_data, aes(
+  x = product, 
+  y = S, 
+  fill = brand)) + 
+  geom_bar(stat = "identity",position = "dodge", colour = "black") + 
+  labs(y = "Saturation Levels", x = "Brands", fill = "Brands") + 
+  ggtitle("Fenty vs. Mac: Saturation Level Differences")
+
---
+++ server.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(bslib)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+
+
+our_data <- read.csv("shades (1).csv")
