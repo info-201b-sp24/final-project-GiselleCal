e8102bbde5c6383afbcdef78043eae965c0be7b8
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-28 19:06:13 -0700

    update

---
+++ Page_3/app.R
@@ -0,0 +1,21 @@
+
+
+library(shiny)
+library(bslib)
+
+#Ui 
+ui <- page_sidebar(
+  title = "Saturation color templet",
+  sidebar = sidebar("Sidebar"),
+  card(
+    card_header("Saturation variation in color range"),
+    "Card Body"
+  )
+)
+
+server <- function(input,output) {
+  
+}
+
+shinyApp(ui = ui, server = server)
+
