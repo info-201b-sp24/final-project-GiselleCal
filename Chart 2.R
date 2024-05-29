43cc4f9d3e45567e38d5d357293fe014eaa4168d
aditim4 <aditim4@uw.edu>
2024-05-29 12:19:27 -0700

    made interactive

--- Chart 2.R
+++ Chart 2.R
@@ -1,14 +1,17 @@
 library(ggplot2)
+library(plotly)
+library(gapminder)
 
 foundation_dataset <- read.csv("https://raw.githubusercontent.com/info-201b-sp24/exploratory-analysis-Aisha03792/main/shades.csv")
 
 filtered_values <- subset(foundation_dataset, brand %in% c("MAC", "Fenty"))
 
 # Box plot to compare ratings for MAC and Fenty shades
-ggplot(filtered_values, aes(x=brand, y=H, fill=brand)) +
+p2 <- ggplot(filtered_values, aes(x=brand, y=H, fill=brand)) +
   geom_boxplot() +
   theme_minimal() +
   labs(title="Box Plot of Ratings for MAC and Fenty Shades", x="Brand", y="Hue")
+ggplotly(p2)
 
 
 
