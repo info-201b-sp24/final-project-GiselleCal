d202e78af62639ce59e2b59de1d91ff79e9a8688
aditim4 <aditim4@uw.edu>
2024-05-29 12:16:43 -0700

    Made interactive

--- Chart 2.R
+++ Chart 2.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-foundation_dataset <- read.csv("shades.csv")
+foundation_dataset <- read.csv("https://raw.githubusercontent.com/info-201b-sp24/exploratory-analysis-Aisha03792/main/shades.csv")
 
 filtered_values <- subset(foundation_dataset, brand %in% c("MAC", "Fenty"))
 
