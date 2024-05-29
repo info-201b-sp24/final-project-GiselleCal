d3c67decd6f9b61ef79429961ce7845f9d994565
Aisha03972 <orcadolfin2005@gmail.com>
2024-05-29 12:14:37 -0700

    updated

--- Chart 3 copy.R
+++ Chart 3 copy.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(plotly)
 library(gapminder)
 
-foundation_dataset <- read.csv("shades.csv")
+foundation_dataset <- read.csv("https://raw.githubusercontent.com/info-201b-sp24/exploratory-analysis-Aisha03792/main/shades.csv")
 
 #filtering data just to get the two brands!
  
@@ -11,12 +11,11 @@ filtered_data <- subset(foundation_dataset, brand %in% c("Fenty", "MAC"))
 
 # Bar graph: Comparing the difference saturation levels between Fenty and Mac. 
 
-p <- gapminder %>%
-ggplot(data = filtered_data, aes(
+p2 <- ggplot(data = filtered_data, aes(
   x = product, 
   y = S, 
   fill = brand)) + 
   geom_bar(stat = "identity",position = "dodge", colour = "black") + 
   labs(y = "Saturation Levels", x = "Brands", fill = "Brands") + 
   ggtitle("Fenty vs. Mac: Saturation Level Differences")
-ggplotly(p)
+ggplotly(p2)
