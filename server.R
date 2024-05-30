31ee1269e4d98adfc8bfa1252748e57a50c55c44
iamsusuu <166072879+iamsusuu@users.noreply.github.com>
2024-05-29 19:25:48 -0700

    Update server.R

--- server.R
+++ server.R
@@ -103,7 +103,7 @@ server <- function(input, output, session) {
 
 server <- function(input, output) {
   filtered_data <- reactive({
-    subset(Foundation_dataset, brand %in% input$brands)
+    subset(foundation_dataset, brand %in% input$brands)
   })
   
   output$histogramPlot <- renderPlotly({
